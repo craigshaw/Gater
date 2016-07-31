0347a22f5c7433d7be08fa04d1a47e8055c5e615
Craig Shaw <craig@craigshaw.com>
2016-07-31 21:58:37 +0100

    Coding standards

--- Gater/Environment/Win32.cs
+++ Gater/Environment/Win32.cs
@@ -20,6 +20,15 @@ namespace Gater.Environment
         [DllImport("user32.dll")]
         public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
+        /// <summary>
+        /// Retrieves information about the specified window. The function also retrieves the 32-bit (DWORD) value at the specified offset into the extra window memory.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window and, indirectly, the class to which the window belongs.</param>
+        /// <param name="nIndex">The zero-based offset to the value to be retrieved. Valid values are in the range zero through the number of bytes of extra window memory, minus four; for example, if you specified 12 or more bytes of extra memory, a value of 8 would be an index to the third 32-bit integer. To retrieve any other value, specify one of the following values.</param>
+        /// <returns>
+        /// If the function succeeds, the return value is the requested value.
+        /// If the function fails, the return value is zero.To get extended error information, call GetLastError.
+        /// If SetWindowLong has not been called previously, GetWindowLong returns zero for values in the extra window or class memory.</returns>
         [DllImport("user32.dll", SetLastError = true)]
         public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
     }
--- Gater/Framework/GameState.cs
+++ Gater/Framework/GameState.cs
@@ -6,23 +6,32 @@ namespace Gater.Framework
 {
     public abstract class GameState : IState
     {
-        private IList<IEnumerator> activeCoroutines;
-        private readonly IStateManager stateManager;
+        private readonly IList<IEnumerator> _activeCoroutines;
+        private readonly IStateManager _stateManager;
+
+        protected IStateManager StateManager
+        {
+            get
+            {
+                return _stateManager;
+            }
+        }
 
         public GameState(IStateManager stateManager)
         {
             if (stateManager == null)
                 throw new ArgumentNullException("stateManager");
 
-            this.stateManager = stateManager;
-            activeCoroutines = new List<IEnumerator>();
+            _stateManager = stateManager;
+            _activeCoroutines = new List<IEnumerator>();
         }
-
-        protected IStateManager StateManager { get { return stateManager; } }
 
         protected void RegisterNextState(IState nextState)
         {
-            stateManager.RegisterNextState(nextState);
+            if (nextState == null)
+                throw new ArgumentNullException("nextState");
+
+            _stateManager.RegisterNextState(nextState);
         }
 
         protected void StartCoroutine(IEnumerator coroutine)
@@ -30,21 +39,21 @@ namespace Gater.Framework
             if (coroutine == null)
                 throw new ArgumentNullException("coroutine");
 
-            activeCoroutines.Add(coroutine);
+            _activeCoroutines.Add(coroutine);
         }
 
         protected void RunCoroutines()
         {
             List<IEnumerator> completedCoroutines = new List<IEnumerator>();
 
-            foreach (var coroutine in activeCoroutines)
+            foreach (var coroutine in _activeCoroutines)
             {
                 if (!coroutine.MoveNext())
                     completedCoroutines.Add(coroutine);
             }
 
             foreach (var coroutine in completedCoroutines)
-                activeCoroutines.Remove(coroutine);
+                _activeCoroutines.Remove(coroutine);
         }
 
         public abstract void Initialise();
